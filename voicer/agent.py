c6e2f514068f74976e59a3dad3b148e97be4ef9f
Adam Smith <the.economy@gmail.com>
2024-10-26 12:27:24 -0700

    initial commit

---
+++ voicer/agent.py
@@ -0,0 +1,71 @@
+from __future__ import annotations
+
+import logging
+from dotenv import load_dotenv
+
+from livekit import rtc
+from livekit.agents import (
+    AutoSubscribe,
+    JobContext,
+    WorkerOptions,
+    cli,
+    llm,
+)
+from livekit.agents.multimodal import MultimodalAgent
+from livekit.plugins import openai
+
+
+load_dotenv(dotenv_path=".env.local")
+logger = logging.getLogger("my-worker")
+logger.setLevel(logging.INFO)
+
+
+async def entrypoint(ctx: JobContext):
+    logger.info(f"connecting to room {ctx.room.name}")
+    await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
+
+    participant = await ctx.wait_for_participant()
+
+    run_multimodal_agent(ctx, participant)
+
+    logger.info("agent started")
+
+
+def run_multimodal_agent(ctx: JobContext, participant: rtc.RemoteParticipant):
+    logger.info("starting multimodal agent")
+
+    model = openai.realtime.RealtimeModel(
+        instructions=(
+            "You are a voice assistant created by LiveKit. Your interface with users will be voice. "
+            "You should use short and concise responses, and avoiding usage of unpronouncable punctuation. "
+            "You were created as a demo to showcase the capabilities of LiveKit's agents framework, "
+            "as well as the ease of development of realtime AI prototypes. You are currently running in a "
+            "LiveKit Sandbox, which is an environment that allows developers to instantly deploy prototypes "
+            "of their realtime AI applications to share with others."
+        ),
+        modalities=["audio", "text"],
+        voice="echo"
+    )
+    assistant = MultimodalAgent(model=model)
+    assistant.start(ctx.room, participant)
+
+    session = model.sessions[0]
+    session.conversation.item.create(
+        llm.ChatMessage(
+            role="assistant",
+            content="""
+            Please begin the interaction with the user in a manner consistent with your instructions.
+            Mention that you are a voice assistant working for SDx, operating out of the Launch Factory, before 
+            listening for the user's instructions.
+            """,
+        )
+    )
+    session.response.create()
+
+
+if __name__ == "__main__":
+    cli.run_app(
+        WorkerOptions(
+            entrypoint_fnc=entrypoint,
+        )
+    )
