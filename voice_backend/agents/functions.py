828f4bdd8d0c1d3747eb614755fe0b1c15d934ba
Rick Moy <rickmoy@gmail.com>
2025-02-08 15:10:34 -0800

    slides function

--- voice_backend/agents/functions.py
+++ voice_backend/agents/functions.py
@@ -27,3 +27,24 @@ class AssistantFnc(llm.FunctionContext):
                     return f"The answer to the question is {cal_data}."
                 else:
                     raise f"Failed to get cal data, status code: {response.status}"
+
+    @llm.ai_callable()
+    async def modify_slides(
+        self,
+        # by using the Annotated type, arg description and type are available to the LLM
+        instructions: Annotated[
+            str, llm.TypeInfo(description="Instructions on how to create or modify slides on a topic.")
+        ],
+    ):
+        """Instructions on how to create or modify slides on a topic."""
+        logger.info("manipulating slides info")
+        url = f"http://localhost:8000/slides"
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, json={"instructions": instructions}) as response:
+                if response.status == 200:
+                    slides_data = await response.text()
+                    # response from the function call is returned to the LLM
+                    # as a tool response. The LLM's response will include this data
+                    return {"slides": slides_data}
+                else:
+                    raise f"Failed  status code: {response.status}"
